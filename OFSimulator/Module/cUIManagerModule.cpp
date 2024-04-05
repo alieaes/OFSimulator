c541df5112bcf9f99b84ddec8f0c8f5d29547026
hogyun.yoon <alieaes54@gmail.com>
2024-04-05 18:13:49 +0900

    추가 : UI 관리를 위한 UI 매니징 모듈 추가

---
+++ OFSimulator/Module/cUIManagerModule.cpp
@@ -0,0 +1,39 @@
+﻿#include "stdafx.h"
+
+#include "Module/cUIManagerModule.hpp"
+
+#include "Base/ExtConfig.hpp"
+
+cUIManagerModule::cUIManagerModule( QObject* parent )
+    : QObject( parent )
+{
+}
+
+cUIManagerModule::~cUIManagerModule()
+{
+}
+
+bool cUIManagerModule::NotifyModule( const XString& sNotifyJobs )
+{
+    return true;
+}
+
+bool cUIManagerModule::moduleInit()
+{
+    return true;
+}
+
+bool cUIManagerModule::moduleStart()
+{
+    return true;
+}
+
+bool cUIManagerModule::moduleStop()
+{
+    return true;
+}
+
+bool cUIManagerModule::moduleFinal()
+{
+    return true;
+}
---
+++ OFSimulator/Module/cUIManagerModule.hpp
@@ -0,0 +1,60 @@
+﻿#pragma once
+
+#ifndef __HDR_OF_UI_MANAGER_MODULE__
+#define __HDR_OF_UI_MANAGER_MODULE__
+
+#include "OFSimulator.h"
+#include "Module/EXModuleBase.hpp"
+
+#include "UI/QMainWorld.hpp"
+#include "UI/QCharacter.hpp"
+#include "UI/QLoadingPage.hpp"
+#include "UI/QMakeProfile.hpp"
+#include "UI/QTimeController.hpp"
+
+class cUIManagerModule : public QObject, public Ext::Module::cModuleBase
+{
+    Q_OBJECT
+
+public:
+    cUIManagerModule( QObject* parent );
+    ~cUIManagerModule();
+
+    void                                     SetMainWindow( OFSimulator* of ) { _parent = of; }
+    void                                     SetWorld( QMainWorld* world ) { _world = world; }
+    void                                     SetLoadingPage( QLoadingPage* page ) { _loadingPage = page; }
+    void                                     SetProfilePage( QMakeProfile* profile ) { _profile = profile; }
+    void                                     SetCharacter( QCharacter* character ) { _character = character; }
+    void                                     SetTimeController( QTimeController* tm ) { _tmController = tm; }
+
+    OFSimulator*                             GetMainWindow() const { return _parent; }
+    QMainWorld*                              GetMainWorld() const { return _world; }
+    QLoadingPage*                            GetLoadingPage() const { return _loadingPage; }
+    QMakeProfile*                            GetProfilePage() const { return _profile; }
+    QCharacter*                              GetCharacter() const { return _character; }
+    QTimeController*                         GetTimeController() const { return _tmController; }
+
+    bool                                     NotifyModule( const XString& sNotifyJobs ) override;
+
+protected:
+    bool                                     moduleInit() override;
+    bool                                     moduleStart() override;
+    bool                                     moduleStop() override;
+    bool                                     moduleFinal() override;
+
+private:
+    OFSimulator*                             _parent       = NULLPTR;
+
+    QMainWorld*                              _world        = NULLPTR;
+
+    QLoadingPage*                            _loadingPage  = NULLPTR;
+
+    QMakeProfile*                            _profile      = NULLPTR;
+
+    QCharacter*                              _character    = NULLPTR;
+
+    QTimeController*                         _tmController = NULLPTR;
+};
+
+
+#endif
