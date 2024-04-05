90921648ddd4393211d3e1581ec20e1789956f98
hogyun.yoon <alieaes54@gmail.com>
2024-04-05 18:10:55 +0900

    추가 : 모듈 매니징을 위한 모듈 Define 파일 추가

--- OFSimulator/def/OFDefines.hpp
+++ OFSimulator/def/OFDefines.hpp
@@ -1,3 +1,7 @@
 ﻿#pragma once
 
 const QString OF_BASE_BACKGROUND_COLOR = "#e0dbd5";
+
+const QString OF_MODULE_UI_MANAGER = "UIMANAGER";
+const QString OF_MODULE_CHARACTER  = "CHARACTER";
+const QString OF_MODULE_SIMULATOR  = "SIMULATOR";
